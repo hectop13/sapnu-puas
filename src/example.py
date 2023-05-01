d7a5683e6af848e96d93f4375a418015db719b1c
lusm <loveyousomuch554@gmail.com>
2023-05-02 00:36:03 +0300

    raw example

---
+++ src/example.py
@@ -0,0 +1,30 @@
+class Controller:
+  ...
+
+class Model:
+  ...
+
+class View:
+  ...
+
+
+def main():
+  while 1:
+    print("1. Encrypt")
+    print("2. Decrypt")
+    print("3. Quit")
+    user_choice = input("Enter number of your choice: ")
+    
+    # logic for every choice below
+    if user_choice == "1":
+      break
+    elif user_choice == "2":
+      break
+    elif user_choice == "3":
+      break
+    else:
+      print("Ты за кого меня держишь, козлина? Pick 1, 2 or 3.")
+
+
+if __name__ == "__main__":
+  main()
